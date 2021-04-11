cb3a8649cd6b34eb78c8d9b06453f8e4b738196e
KryvosheienkoY <j.kryvosheienko@gmail.com>
2021-04-11 05:44:21 +0300

    Added PasswordEncrypter

--- BudgetSystemLab2/Services/AuthenticationService.cs
+++ BudgetSystemLab2/Services/AuthenticationService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using BudgetSystemLab2.Utils;
 using DataStorage;
 
 namespace BudgetSystemLab2.Services
@@ -10,14 +12,30 @@ namespace BudgetSystemLab2.Services
     public class AuthenticationService
     {
         private FileDataStorage<DBUser> _storage = new FileDataStorage<DBUser>();
+        private PasswordEncrypter encrypter = new PasswordEncrypter(20);
 
         public async Task<User> AuthenticateAsync(AuthenticationUser authUser)
         {
             if (String.IsNullOrWhiteSpace(authUser.Login) || String.IsNullOrWhiteSpace(authUser.Password))
                 throw new ArgumentException("Login or Password is Empty");
             var users = await _storage.GetAllAsync();
-            var dbUser = users.FirstOrDefault(user => user.Login == authUser.Login && user.Password == authUser.Password);
-            if (dbUser == null)
+            //hash user`s pswd
+            //byte[] pswd = Encoding.ASCII.GetBytes(authUser.Password);
+            //byte[] salt = Encoding.ASCII.GetBytes(encrypter.Salt);
+            //byte[] encryptedPswd =encrypter.GenerateSaltedHash(pswd, salt);
+            //var dbUser= new DBUser("","","","","");
+            //foreach (DBUser user in users)
+            //{
+            //    if(user.Login == authUser.Login)
+            //    {
+            //        if (encrypter.CompareByteArrays(encryptedPswd, Encoding.ASCII.GetBytes(user.Password)))
+            //        {
+            //            dbUser = user;
+            //        }
+            //    }
+            //}
+            var dbUser = users.FirstOrDefault(user => user.Login == authUser.Login && user.Password==authUser.Password);
+            if (dbUser.Login == "")
                 throw new Exception("Wrong Login or Password");
             LoginedUser.User = dbUser;
             return new User(dbUser.Guid, dbUser.FirstName, dbUser.LastName, dbUser.Email, dbUser.Login);
@@ -32,6 +50,12 @@ namespace BudgetSystemLab2.Services
                 throw new Exception("User already exists");
             if (String.IsNullOrWhiteSpace(regUser.Login) || String.IsNullOrWhiteSpace(regUser.Password) || String.IsNullOrWhiteSpace(regUser.LastName))
                 throw new ArgumentException("Login, Password or Last Name is Empty");
+            ////encrypt pswd
+            //byte[] pswd = Encoding.ASCII.GetBytes(regUser.Password);
+            //byte[] salt = Encoding.ASCII.GetBytes(encrypter.Salt);
+            //string encryptedPswd = Encoding.ASCII.GetString(encrypter.GenerateSaltedHash(pswd, salt)); 
+            //dbUser = new DBUser(regUser.LastName + regUser.FirstName, regUser.LastName, regUser.Email,
+            //    regUser.Login, encryptedPswd); 
             dbUser = new DBUser(regUser.LastName + regUser.FirstName, regUser.LastName, regUser.Email,
                 regUser.Login, regUser.Password);
             await _storage.AddOrUpdateAsync(dbUser);
---
+++ BudgetSystemLab2/Utils/PasswordEncrypter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetSystemLab2.Utils
+{
+    public class PasswordEncrypter
+    {
+        public string Salt;
+        public PasswordEncrypter(int size)
+        {
+            Salt = CreateSalt(size);
+        }
+        private  string CreateSalt(int size)
+        {
+            //Generate a cryptographic random number.
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            byte[] buff = new byte[size];
+            rng.GetBytes(buff);
+
+            // Return a Base64 string representation of the random number.
+            return Convert.ToBase64String(buff);
+        }
+
+
+        public  byte[] GenerateSaltedHash(byte[] plainText, byte[] salt)
+        {
+            HashAlgorithm algorithm = new SHA256Managed();
+
+            byte[] plainTextWithSaltBytes =
+              new byte[plainText.Length + salt.Length];
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                plainTextWithSaltBytes[i] = plainText[i];
+            }
+            for (int i = 0; i < salt.Length; i++)
+            {
+                plainTextWithSaltBytes[plainText.Length + i] = salt[i];
+            }
+
+            return algorithm.ComputeHash(plainTextWithSaltBytes);
+        }
+        public  bool CompareByteArrays(byte[] array1, byte[] array2)
+        {
+            if (array1.Length != array2.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i] != array2[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
