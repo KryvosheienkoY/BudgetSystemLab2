8469a990e4c1280a2db9e9680a1c4a6016b9baef
KryvosheienkoY <j.kryvosheienko@gmail.com>
2021-04-11 14:10:48 +0300

    problem with xUnit project compatibility fixed

---
+++ Tests/TransactionTest.cs
@@ -0,0 +1,53 @@
+﻿using BudgetSystemLab2;
+using System;
+using System.Drawing;
+using Xunit;
+namespace BudgetTests
+{
+    public class TransactionTest
+    {
+        [Fact]
+        public void ValidateValid()
+        {
+            //Arrange
+            var transaction = new Transaction(Guid.NewGuid())
+            {
+                Sum = -40m,
+                Description = "Cinema spendings in 40 m",
+                CurrencyOfTransaction = Enums.Currency.UAH,
+                Category = new Category(Guid.NewGuid())
+                {
+                    Name = "Cinema",
+                    Description = "Cinema spendings",
+                    Color = Enums.Colors.Orange,
+                    CategoryIconPath = "src/icon1.png",
+                },
+                DateTime = DateTime.Now,
+                FilePath = "/src/photo.png"
+            };
+            var transaction1 = new Transaction(Guid.NewGuid())
+            {
+                Sum = -40m,
+                Description = "Cinema spendings in 40 m",
+                CurrencyOfTransaction = Enums.Currency.UAH,
+                Category = new Category(Guid.NewGuid())
+                {
+                    Name = "Cinema",
+                    Description = "Cinema spendings",
+                    Color = Enums.Colors.Orange,
+                    CategoryIconPath = "src/icon1.png",
+                },
+                DateTime = DateTime.Now,
+            };
+
+            //Act
+            var actual = transaction.Validate();
+            var actual1 = transaction1.Validate();
+
+            //Assert
+            Assert.True(actual);
+            Assert.True(actual1);
+        }
+
+    }
+}
